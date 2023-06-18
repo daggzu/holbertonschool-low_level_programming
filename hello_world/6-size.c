f738270e93f395df16b6e4978ded0e915ffbd554
daggzu <diegog8603@gmail.com>
2023-06-18 23:42:27 +0000

    lol

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
 
 /**
-*
-* main - Prints the size of various types based
-* on the computer it is compiled and run on
-*
-* Return: Always 0
-*/
+ * main - Prints the size of various types based
+ * on the computer it is compiled and run on
+ *
+ * Return: Always 0
+ */
 int main(void)
 {
         printf("Size of a char: %zu byte(s)\n", sizeof(char));
@@ -17,3 +16,5 @@ int main(void)
 
         return (0);
 }
+
+
