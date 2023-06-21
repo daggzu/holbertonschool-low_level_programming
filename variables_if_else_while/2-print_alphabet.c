24582beefdb5cd164e0c0ca53d3bc9b3ab694c7d
daggzu <diegog8603@gmail.com>
2023-06-21 18:36:18 +0000

    lol

--- variables_if_else_while/2-print_alphabet.c
+++ variables_if_else_while/2-print_alphabet.c
@@ -7,8 +7,8 @@
 
 int main(void)
 {
-	char alphabet
-	for(alphabet = 'a'; alphabet <= 'z'; alphabet++);
+	char alphabet;
+	for(alphabet = 'a'; alphabet <= 'z'; alphabet++)
 	{
 	putchar(alphabet);
 	
