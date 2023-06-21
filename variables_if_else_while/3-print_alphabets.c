79aaf37037047a833dcc59811aa85a10c8f0e28f
daggzu <diegog8603@gmail.com>
2023-06-21 20:01:45 +0000

    LOL

--- variables_if_else_while/3-print_alphabets.c
+++ variables_if_else_while/3-print_alphabets.c
@@ -12,11 +12,15 @@ int main(void)
 	char lowercase;
 
 	for (lowercase = "a"; lowercase <= "z"; lowercase++)
-	putchar(lowercase);
-
+	{
+		putchar(lowercase);
+	}
+	
 	for (lowercase = "A"; lowercase <= "Z"; lowercase++)
-	putchar(lowercase);
+	{
+		putchar(lowercase);
+	}
 	putchar('\n');
-
+	
 	return (0);
 }
