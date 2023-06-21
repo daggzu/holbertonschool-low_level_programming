56aa4d6029fbc8c7491681025de34e0b254096f0
daggzu <diegog8603@gmail.com>
2023-06-21 19:53:56 +0000

    LOL

---
+++ variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ * Return: always 0
+ */
+
+int main(void)
+{
+	char lowercase;
+
+	for (lowercase = "a"; lowercase <= "z"; lowercase++)
+	putchar(lowercase);
+
+	for (lowercase = "A"; lowercase <= "Z"; lowercase++)
+	putchar(lowercase);
+	putchar('\n');
+
+	return (0)
+}
