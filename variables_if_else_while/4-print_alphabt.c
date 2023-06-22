13f0a8255da832002afe06d2e5760e63870d307f
daggzu <diegog8603@gmail.com>
2023-06-22 13:47:26 +0000

    lol

---
+++ variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * Return: always 0
+ */
+
+int main(void)
+{
+	char lower, e, q;
+	e = 'e';
+	q = 'q';
+
+	for (lower = 'a'; lower <= 'z'; lower++)
+	{
+		if (lower != e && lower != q)
+		putchar(lower);
+	}
+	putchar('n');
+	return (0);
+}
+
+
+
