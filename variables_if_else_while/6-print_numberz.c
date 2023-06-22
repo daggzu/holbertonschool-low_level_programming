e078619859b43f8e7b2b106f982819e82a57e038
daggzu <diegog8603@gmail.com>
2023-06-22 14:47:14 +0000

    lol

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Return: always 0
+ */
+
+int main(void)
+{
+	int s;
+
+	for (s = 0; s <= 9 ; s++)
+	putchar(s);
+	putchar('\n');
+	
+	return (0);
+}
+
