c778b2b2808a6b9de58a3b4cb6b49164def99b55
daggzu <diegog8603@gmail.com>
2023-06-23 14:58:59 +0000

    ol

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -1,4 +1,4 @@
-
+#include <main.h>
 /**
  * main - entry point
  * Return: always 0
@@ -6,9 +6,13 @@
 
 int main(void)
 {
-
-	{
-	printf("_putchar\n");
-	}
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
 	return (0);
 }
