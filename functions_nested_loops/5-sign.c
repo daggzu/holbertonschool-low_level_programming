8efe4a4b2f23cf010bb40b481c18d1e36e43f17e
daggzu <diegog8603@gmail.com>
2023-06-24 01:39:38 +0000

    lol

---
+++ functions_nested_loops/5-sign.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * print_sign - Prints the sifn of a number
+ * @n: The number to be checked i think
+ * Return: 1 for posotitve output, -1 for negative output or zero for anythung else
+ */
+
+int print_sign(int n)
+
+{
+	if (n > 0)
+	{
+	_putchar(43);
+	return (1);
+	}
+	else if (n < 0)
+	{
+	_putchar(45);
+	return (-1);
+	}
+	else
+	{
+	_putchar(48);
+	return (0);
+	}
+}
+
