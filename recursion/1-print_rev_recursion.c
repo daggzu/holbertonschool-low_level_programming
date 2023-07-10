242145a2257f72494c1657536b343ada8575fce3
daggzu <diegog8603@gmail.com>
2023-07-10 21:07:25 +0000

    lol

---
+++ recursion/1-print_rev_recursion.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _print_rev_recursion - Prints a string in reverse
+ * @s: Pointer to string
+ */
+void _print_rev_recursion(char *s)
+{
+	if (!*s)
+		return;
+	_print_rev_recursion(s + 1);
+	_putchar(*s);
+}
