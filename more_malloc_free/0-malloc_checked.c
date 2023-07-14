129036a5e929298f5a9bfe269933d89223bee5c9
daggzu <diegog8603@gmail.com>
2023-07-14 18:26:41 +0000

    lol

--- more_malloc_free/0-malloc_checked.c
+++ more_malloc_free/0-malloc_checked.c
@@ -1,30 +1,24 @@
-#include "main.h"
-#include <stdio.h>
 #include <stdlib.h>
-#include <limits.h>
-/**
- * main - check code
- *
- * Return: Always 0
- */
-int main(void)
-{
-	char *c;
-	int *i;
-	float *f;
-	double *d;
+#include <stdio.h>
+#include "main.h"
 
-	c = malloc_checked(sizeof(char) * 1024);
-	printf("%p\n", (void *)c);
-	i = malloc_checked(sizeof(int) * 402);
-	printf("%p\n", (void *)i);
-	f = malloc_checked(sizeof(float) * 100000000);
-	printf("%p\n", (void *)f);
-	d = malloc_checked(INT_MAX);
-	printf("%p\n", (void *)d);
-	free(c);
-	free(i);
-	free(f);
-	free(d);
-	return (0);
+/**
+ * malloc_checked - function to allocates memory using malloc
+ * @b: memory allocated
+ * Return: a pointer to the allocated memory
+ */
+
+void *malloc_checked(unsigned int b)
+{
+	char *ptr;
+	/* Allocate memory of the specified size */
+	ptr = malloc(b);
+	/* Check if the malloc failed. */
+	if (ptr == NULL)
+	{
+		/* The malloc failed */
+		exit(98);
+	}
+	/* Return the pointer to the allocated memory */
+	return (ptr);
 }
