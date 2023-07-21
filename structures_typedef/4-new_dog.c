eb41e7528cda5554f1ff4fade43f4b1009d78656
daggzu <diegog8603@gmail.com>
2023-07-21 13:59:04 +0000

    lol

---
+++ structures_typedef/4-new_dog.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include "dog.h"
+#include <string.h>
+#include <stdlib.h>
+
+/**
+ * new_dog - function that creates a new dog.
+ * @name: name
+ * @age: age
+ * @owner: name of owner
+ * Return: new dog
+ */
+
+dog_t *new_dog(char *name, float age, char *owner)
+{
+
+	dog_t *p = malloc(sizeof(dog_t));
+
+	if (p == NULL)
+	{
+		return (NULL);
+	}
+
+	p->name = malloc(strlen(name) * sizeof(name));
+
+	if (p->name == NULL)
+	{
+		free(p);
+		return (NULL);
+	}
+
+	p->owner = malloc(strlen(owner) * sizeof(owner));
+
+	if (p->owner == NULL)
+	{
+		free(p->name);
+		free(p);
+		return (NULL);
+	}
+	
+	strcpy(p->name, name);
+	p->age = age;
+	strcpy(p->owner, owner);
+
+	return (p);
+}
