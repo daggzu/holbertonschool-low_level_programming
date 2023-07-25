e9f291c65ad9968089d6437b0ab7c8ed78640e31
daggzu <diegog8603@gmail.com>
2023-07-25 15:02:36 +0000

    lol

---
+++ function_pointers/1-array_iterator.c
@@ -0,0 +1,21 @@
+#include "function_pointers.h"
+/**
+ * array_iterator - func that executes a func given as a parameter
+ * @array: parameter for array
+ * @size: parameter for size
+ * @action: paramter for action
+ * Return: Nothing.
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	unsigned int i = 0;
+
+	if (array != NULL && action != NULL && size > 0)
+	{
+		while (i < size)
+		{
+			action(array[i]);
+			i++;
+		}
+	}
+}
