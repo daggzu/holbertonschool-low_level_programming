117e34e6c5dc261b92f1cc4d24a0242d2c01021a
daggzu <diegog8603@gmail.com>
2023-08-11 14:40:21 +0000

    lol

--- singly_linked_lists/lists.h
+++ singly_linked_lists/lists.h
@@ -1,3 +1,5 @@
+#ifndef _HEADER_
+#define _HEADER_
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -21,3 +23,4 @@ size_t list_len(const list_t *h);
 list_t *add_node(list_t **head, const char *str);
 list_t *add_node_end(list_t **head, const char *str);
 void free_list(list_t *head);
+#endif
